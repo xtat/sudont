d25e785dfbc0869c636ba674e42272885107263d
Todd Troxell <ttroxell@debian.org>
2009-04-29 16:13:10 -0400

    Mu.  Thanks Kate!

--- sudont.c
+++ sudont.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 
+/*
+ A monk once asked master Chao-chou, "Does a dog have Buddha-nature or not?"
+
+ Chao-chou said, "Mu"
+*/
+
 int main(int argc, char * argv)
 {
     exit(0);
